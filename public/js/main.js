e2ada9fae56f12c0b9325d81ca0a6166e400176e
Shikaga <shikaga@gmail.com>
2013-07-28 16:43:30 +0100

    Forgot these...

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,15 @@ function sub(number1,number2)
 }
 
 console.log(sub(5,4));
+
+var socket = io.connect(window.location.href);
+socket.on('chat', function (data) {
+	console.log(data);
+	var label = document.getElementById("textLabel");
+	label.innerHTML = data.text;
+});
+
+function setText() {
+	var value = document.getElementById("textBox").value;
+	socket.emit("chat", {text: value});
+}
